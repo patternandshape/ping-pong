55ed5c3ba3a6853f1f740767f201b2d68d7c2583
Molly Curtin <patternandshape@gmail.com>
2016-01-22 10:50:50 -0800

    add working spec file

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,36 @@
 var pingPong = function(number){
-  if (number % 3 === ping || number % 5 === pong);
+  if ((number % 3 === 0) || (number % 5 === 0)){
   return true;
 } else {
   return false;
-}
+};
+};
 
 $(document).ready(function(){
-  $("form#formInput").submit(function(event) {
+  $("form#formInput").submit(function(event){
     $("ul").empty();
     var number = parseInt($("input#numberEntry").val());
+
+  for (i=1; i<=number; i++)
+    if (i % 3 === 0 && i % 5 === 0){
+    $("ul").append("<li>pingpong</li>");
+
+  } else if (i % 3 === 0){
+    $("ul").append("<li>ping</li>");
+
+  } else if (i % 5 === 0){
+    $("ul").append("<li>pong</li>");
+  } else {
+    $("ul").append("<li>"+i+"</li>");
+  }
+
+
+
+
+
+
+
+    event.preventDefault();
+
+  });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,14 @@
 describe ("pingPong", function() {
   it("will return ping when a number divisible by 3 is entered", function () {
-    expect(pingPong(3)).to.equal("ping");
+    expect(pingPong("3")).to.equal(true);
   });
   it("will return pong when a number divisible by 5 is entered", function () {
-    expect(pingPong(5)).to.equal("pong");
+    expect(pingPong("5")).to.equal(true);
   });
   it("will return pingpong when a number divisible by 15 is entered", function () {
-    expect(pingPong(15)).to.equal("pingpong");
+    expect(pingPong("15")).to.equal(true);
   });
   it("will return the number entered when it is not divisible by 3, 5, nor 15", function () {
-    expect(pingPong(4)).to.equal("4");
+    expect(pingPong("4")).to.equal(false);
   });
 });
