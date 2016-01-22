c5c59250fa9e8b3067f3d2c7af7b3ad5de884f0d
Molly Curtin <patternandshape@gmail.com>
2016-01-22 13:31:57 -0800

    add color shifts to the jumbotron and moved some text around for better legibility

--- spec/specs.js
+++ spec/specs.js
@@ -8,7 +8,7 @@ describe ("pingPong", function() {
   it("will return pingpong when a number divisible by 15 is entered", function () {
     expect(pingPong("15")).to.equal(true);
   });
-  it("will return the number entered when it is not divisible by 3, 5, nor 15", function () {
+  it("will return the number entered when the number is not divisible by 3, 5, nor 15", function () {
     expect(pingPong("4")).to.equal(false);
   });
 });
