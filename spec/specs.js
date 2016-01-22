2b1365d6d9870ba79a7e565cfb5556bc8a9f9923
Molly Curtin <patternandshape@gmail.com>
2016-01-22 14:03:53 -0800

    add proper indentation to all files in the project

--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,14 @@
-describe ("pingPong", function() {
-  it("will return ping when a number divisible by 3 is entered", function () {
+describe("pingPong", function() {
+  it("will return ping when a number divisible by 3 is entered", function() {
     expect(pingPong("3")).to.equal(true);
   });
-  it("will return pong when a number divisible by 5 is entered", function () {
+  it("will return pong when a number divisible by 5 is entered", function() {
     expect(pingPong("5")).to.equal(true);
   });
-  it("will return pingpong when a number divisible by 15 is entered", function () {
+  it("will return pingpong when a number divisible by 15 is entered", function() {
     expect(pingPong("15")).to.equal(true);
   });
-  it("will return the number entered when the number is not divisible by 3, 5, nor 15", function () {
+  it("will return the number entered when the number is not divisible by 3, 5, nor 15", function() {
     expect(pingPong("4")).to.equal(false);
   });
 });
